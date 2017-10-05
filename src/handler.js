eda0d2797fee24e106befe3512c790ad46a4e74d
André Bonna <andre.bonna@pminova.com>
2017-10-05 16:48:19 -0300

    Include IPTC data parse from image

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 import sharp from 'sharp';
 import logger from 'winston';
+import iptc from 'node-iptc';
 
 logger.level = process.env.LOG_LEVEL || 'info';
 
@@ -18,7 +19,11 @@ export function postprocess(event) {
             Bucket: process.env.SOURCE_BUCKET_NAME,
             Key: key
         }).promise()
-            .then((data) => sharp(data.Body).resize(640).toFormat('png').toBuffer())
+            .then((data) => {
+                const result = iptc(data.Body);
+                logger.info('IPTC Data', JSON.parse(Buffer.from(JSON.stringify(result), 'ASCII').toString('utf-8')));
+                return sharp(data.Body).resize(640).toFormat('png').toBuffer();
+            })
             .then(data => s3.putObject({
                 Bucket: process.env.TARGET_BUCKET_NAME,
                 Key: key + '-640.png',
