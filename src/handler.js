9458971201081269bfd89f79f4975d2b68c2935b
André Bonna <andre.bonna@pminova.com>
2017-10-06 10:53:51 -0300

    Implement remove event and handler refactor

--- src/handler.js
+++ src/handler.js
@@ -3,7 +3,10 @@ import sharp from 'sharp';
 import logger from 'winston';
 import iptc from 'node-iptc';
 
-logger.level = process.env.LOG_LEVEL || 'debug';
+logger.level = process.env.LOG_LEVEL || 'info';
+
+//List of widths to resize image - Could be an external parameter
+const WIDTHS = [1024, 640, 320];
 
 const s3 = new AWS.S3({ region: 'us-east-1' });
 const dynamodb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
@@ -17,10 +20,26 @@ function getImageKey(width, key) {
 }
 
 function getTargetURLs(widths, key) {
-    return widths.map((width)=> `https://s3.amazonaws.com/${process.env.TARGET_BUCKET_NAME}/${getImageKey(width, key)}`);
+    return widths
+        .map((width) => `https://s3.amazonaws.com/${process.env.TARGET_BUCKET_NAME}/${getImageKey(width, key)}`);
 }
 
-function resizeAndSave(width, key) {
+function getObjectByKey(key) {
+    return s3.getObject({
+        Bucket: process.env.SOURCE_BUCKET_NAME,
+        Key: key
+    }).promise().then((data) => {
+        // Enrich data with iptc metadata
+        const iptcMetadata = convertASCIItoUTF8(iptc(data.Body));
+        logger.debug('IPTC Data', iptcMetadata);
+        return {
+            ...data,
+            iptcMetadata
+        };
+    });
+}
+
+function resizeAndPutS3Object(width, key) {
     return data => {
         return sharp(data.Body).resize(width).toFormat('png').toBuffer()
             .then(fileBuffer => s3.putObject({
@@ -34,7 +53,39 @@ function resizeAndSave(width, key) {
     };
 }
 
-export function postprocess(event) {
+function removeS3Object(width, key) {
+    return () => s3.deleteObject({
+        Bucket: process.env.TARGET_BUCKET_NAME,
+        Key: getImageKey(width, key)
+    }).promise();
+}
+
+function saveMetadata(data, key) {
+    const { headline, caption, special_instructions } = data.iptcMetadata;
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+            Name: key,
+            Title: headline,
+            Description: caption,
+            URLs: getTargetURLs(WIDTHS, key),
+            Categories: special_instructions && special_instructions.split(',')
+        }
+    };
+    return dynamodb.put(params).promise();
+}
+
+function deleteMetadata(key) {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Key: {
+            Name: key
+        }
+    };
+    return dynamodb.delete(params).promise();
+}
+
+export function create(event) {
     event.Records.forEach((record) => {
         const {
             object: {
@@ -42,38 +93,38 @@ export function postprocess(event) {
             }
         } = record.s3;
 
-        s3.getObject({
-            Bucket: process.env.SOURCE_BUCKET_NAME,
-            Key: key
-        }).promise()
+        let promise = getObjectByKey(key);
 
-            .then((data) => {
-                const iptcMetadata = convertASCIItoUTF8(iptc(data.Body));
-                logger.debug('IPTC Data', iptcMetadata);
-                return {
-                    ...data,
-                    iptcMetadata
-                };
-            })
-            .then(resizeAndSave(1024, key))
-            .then(resizeAndSave(640, key))
-            .then(resizeAndSave(320, key))
-            .then((data) => {
-                logger.debug('Image Saved to target bucket', key);
-                const { headline, caption, special_instructions } = data.iptcMetadata;
-                const params = {
-                    TableName: process.env.DYNAMODB_TABLE,
-                    Item: {
-                        Name: headline,
-                        Description: caption,
-                        URLs: getTargetURLs([1024, 640, 320], key),
-                        Categories: special_instructions && special_instructions.split(',')
-                    }
-                };
-                return dynamodb.put(params).promise();
-            })
-            
-            .then(() => logger.debug('Image Metadata Saved to Table', process.env.DYNAMODB_TABLE))
+        for (const width of WIDTHS) {
+            promise = promise.then(resizeAndPutS3Object(width, key));
+        }
+
+        promise
+            .then(() => logger.info('Image Saved to target bucket', key))
+            .then((data) => saveMetadata(data, key))
+            .then(() => logger.info('Image Metadata Saved to Table', process.env.DYNAMODB_TABLE))
+            .catch(err => logger.error(err, err.stack));
+    });
+}
+
+export function remove(event) {
+    event.Records.forEach((record) => {
+        const {
+            object: {
+                key
+            }
+        } = record.s3;
+
+        let promise = Promise.resolve();
+
+        for (const width of WIDTHS) {
+            promise = promise.then(removeS3Object(width, key));
+        }
+
+        promise
+            .then(() => logger.info('Image Remove from target bucket', key))
+            .then(() => deleteMetadata(key))
+            .then(() => logger.info('Image Metadata Removed from Table', process.env.DYNAMODB_TABLE))
             .catch(err => logger.error(err, err.stack));
     });
 }
